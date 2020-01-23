eafc3121830a4810265a24294bab6f2c67a425e8
Adam Montgomery <amontgomery@patriotgetaways.com>
2020-01-23 16:08:47 -0600

    add resize ability to page fields

--- src/ButterCMS/Model/Page.php
+++ src/ButterCMS/Model/Page.php
@@ -4,13 +4,40 @@ namespace ButterCMS\Model;
 
 class Page extends Model
 {
-    protected
-        $slug,
-        $page_type,
-        $fields;
+    protected $slug;
+    protected $page_type;
+    protected $fields;
 
     public function getField($fieldName, $default = null)
     {
         return isset($this->fields[$fieldName]) ? $this->fields[$fieldName] : $default;
+    }
+
+    public function resizeField($fieldName, $params = null)
+    {
+        $path = $this->fields[$fieldName];
+        if ($params) {
+            $t = explode('/', $path);
+            $filename = $t[count($t) - 1];
+            $path = str_ireplace('cdn.buttercms.com', 'fs.buttercms.com', $path);
+            $commands = $this->resizeCommands($params);
+            $path = str_ireplace($filename, $commands, $path);
+            $path .= "/$filename";
+        }
+        return $path;
+    }
+
+    private function resizeCommands(array $params)
+    {
+        $cntr = 0;
+        $response = 'resize=';
+        foreach ($params as $key => $value) {
+            if ($cntr > 0) {
+                $response .= ',';
+            }
+            $response .= "$key:$value";
+            $cntr++;
+        }
+        return $response;
     }
 }
